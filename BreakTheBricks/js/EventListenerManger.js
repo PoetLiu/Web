3fc916774ef354ef49c482f2afbb37621319d8aa
Albert Liu <457566268@qq.com>
2017-09-01 10:05:24 +0800

    Optimize: Change code to ECMAScript6.0. Using Class.

--- BreakTheBricks/js/EventListenerManger.js
+++ BreakTheBricks/js/EventListenerManger.js
@@ -1,17 +1,19 @@
-function EventListenerManager() {
-    this.liseners = [];
+class EventListenerManager {
+    constructor() {
+        this.liseners = [];
+    };
+
+    addEventListenerTo(target) {
+        let args = Array.prototype.slice.call(arguments);
+        args.shift();
+        target.addEventListener.apply(target, args);
+        this.liseners.push({t: target, args: args});
+    };
+
+    removeAll() {
+        for (let i = 0; i < this.liseners.length; i++) {
+            let o = this.liseners[i];
+            o.t.removeEventListener.apply(o.t, o.args);
+        }
+    };
 }
-
-EventListenerManager.prototype.addEventListenerTo = function (target) {
-    let args = Array.prototype.slice.call(arguments);
-    args.shift();
-    target.addEventListener.apply(target, args);
-    this.liseners.push({t: target, args: args});
-};
-
-EventListenerManager.prototype.removeAll = function () {
-    for (let i = 0; i < this.liseners.length; i++) {
-        let o = this.liseners[i];
-        o.t.removeEventListener.apply(o.t, o.args);
-    }
-};
