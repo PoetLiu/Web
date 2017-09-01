d0eece0c7da391d8ce7f7df044f801fe96e89754
Albert Liu <457566268@qq.com>
2017-09-01 09:38:38 +0800

    Optimize: Change code to ECMAScript6.0. Using Class.

--- BreakTheBricks/js/Ball.js
+++ BreakTheBricks/js/Ball.js
@@ -1,90 +1,90 @@
-function Ball(ctx, x, y, radius, color) {
-    RectImage.call(this, ctx, x, y, radius * 2, radius * 2, color);
-    this.speedX = 4;
-    this.speedY = 4;
-    this.r = radius;
-    this.dragble    = true;
-    this.collideWallDir = 'none';
-}
+class Ball extends RectImage {
+    constructor(ctx, x, y, radius, color) {
+        super(ctx, x, y, radius * 2, radius * 2, color);
+        this.speedX = 4;
+        this.speedY = 4;
+        this.r = radius;
+        this.dragble = true;
+        this.collideWallDir = 'none';
+    };
 
-Ball.prototype = Object.create(RectImage.prototype);
-Ball.prototype.constructor = Ball;
+    draw() {
+        // draw balls
+        if (this.reDraw) {
+            let c = this.ctx;
+            this.clearSelf();
+            c.globalAlpha = this.alpha;
+            c.beginPath();
+            c.arc(this.x + this.r, this.y + this.r, this.r, 0, Math.PI * 2, true);
+            c.fillStyle = this.color;
+            c.fill();
+            this.reDraw = false;
+            this.oldX = this.x;
+            this.oldY = this.y;
+        }
+    };
 
-Ball.prototype.draw = function () {
-    // draw balls
-    if (this.reDraw) {
-        let c = this.ctx;
-        this.clearSelf();
-        c.globalAlpha = this.alpha;
-        c.beginPath();
-        c.arc(this.x+this.r, this.y+this.r, this.r, 0, Math.PI * 2, true);
-        c.fillStyle = this.color;
-        c.fill();
-        this.reDraw = false;
-        this.oldX = this.x;
-        this.oldY = this.y;
-    }
-};
+    onCollide(collideResult) {
+        let c = collideResult;
+        let a = c.area;
 
-Ball.prototype.onCollide = function (collideResult) {
-    let c = collideResult;
-    let a = c.area;
+        // log(c, this);
+        if (a.w >= a.h) {
+            this.bounceY();
+        } else {
+            this.bounceX();
+        }
 
-    // log(c, this);
-    if (a.w >= a.h) {
-        this.bounceY();
-    } else {
-        this.bounceX();
-    }
+        // wait until the ball escaped from target.
+        do {
+            this.update();
+        } while (this.checkCollideWith(c.target).collide);
+    };
 
-    // wait until the ball escaped from target.
-    do {
-        this.update();
-    } while (this.checkCollideWith(c.target).collide);
-};
+    bounceX() {
+        this.speedX *= -1;
+    };
 
-Ball.prototype.bounceX = function () {
-    this.speedX *= -1;
-};
-Ball.prototype.bounceY = function () {
-    this.speedY *= -1;
-};
+    bounceY() {
+        this.speedY *= -1;
+    };
 
-Ball.prototype.onDragTo = function (x, y) {
-    // log(this, x, y);
-    this.moveTo(x-this.r, y-this.r);
-    this.draw();
-};
+    onDragTo(x, y) {
+        // log(this, x, y);
+        this.moveTo(x - this.r, y - this.r);
+        this.draw();
+    };
 
-Ball.prototype.moveTo = function (x, y) {
-    let c = this.ctx.canvas;
-    this.x = x;
-    this.y = y;
-
-    x = numInSection(this.x, 0, c.width - this.w);
-    if (this.x !== x) {
+    moveTo(x, y) {
+        let c = this.ctx.canvas;
         this.x = x;
-        this.bounceX();
-        this.collideWallDir = (x === 0 ? 'left' : 'right');
-    }
-
-    y = numInSection(this.y, 0, c.height - this.h);
-    if (this.y !== y) {
         this.y = y;
-        this.bounceY();
-        this.collideWallDir = (y === 0 ? 'top' : 'bottom');
-    }
-    this.reDraw = (this.x !== this.oldX || this.y !== this.oldY);
-};
 
-Ball.prototype.move = function () {
-    this.moveTo(this.x + this.speedX, this.y + this.speedY);
-};
+        x = numInSection(this.x, 0, c.width - this.w);
+        if (this.x !== x) {
+            this.x = x;
+            this.bounceX();
+            this.collideWallDir = (x === 0 ? 'left' : 'right');
+        }
 
-Ball.prototype.update = function (paused) {
-    if (paused) {
-        return;
-    }
-    this.move();
-    this.draw();
-};
+        y = numInSection(this.y, 0, c.height - this.h);
+        if (this.y !== y) {
+            this.y = y;
+            this.bounceY();
+            this.collideWallDir = (y === 0 ? 'top' : 'bottom');
+        }
+        this.reDraw = (this.x !== this.oldX || this.y !== this.oldY);
+    };
+
+    move() {
+        this.moveTo(this.x + this.speedX, this.y + this.speedY);
+    };
+
+    update(paused) {
+        if (paused) {
+            return;
+        }
+        this.move();
+        this.draw();
+    };
+}
