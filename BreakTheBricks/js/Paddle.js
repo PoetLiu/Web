61a80780c8074b724cdfb8f684b61e1a14ab21f2
Albert Liu <457566268@qq.com>
2017-09-01 10:21:22 +0800

    Optimize: Change code to ECMAScript6.0. Using Class.

--- BreakTheBricks/js/Paddle.js
+++ BreakTheBricks/js/Paddle.js
@@ -1,51 +1,52 @@
-function Paddle(ctx, x, y, width, height, color) {
-    RectImage.call(this, ctx, x, y, width, height, color);
-    this.speedX = 30;
-    this.dragble = true;
+class Paddle extends RectImage {
+    constructor(ctx, x, y, width, height, color) {
+        super(ctx, x, y, width, height, color);
+        this.speedX = 30;
+        this.dragble = true;
+    };
+
+    onDragTo(x) {
+        this.moveTo(x - this.w / 2, this.y);
+        this.draw();
+    };
+
+    moveTo(x, y) {
+        let c = this.ctx.canvas;
+        this.x = numInSection(x, 0, c.width - this.w);
+        this.y = y;
+        this.reDraw = (this.x !== this.oldX || this.y !== this.oldY);
+    };
+
+    moveLeft() {
+        this.moveTo(this.x - this.speedX, this.y);
+    };
+
+    movedRight() {
+        this.moveTo(this.x + this.speedX, this.y);
+    };
+
+    onCollide() {
+        //log(area);
+        this.reDraw = true;
+    };
+
+    draw() {
+        if (this.reDraw) {
+            // log('paddle:redraw.');
+            this.clearSelf();
+            let c = this.ctx;
+            c.fillStyle = this.color;
+            c.fillRect(this.x, this.y, this.w, this.h);
+            this.reDraw = false;
+            this.oldX = this.x;
+            this.oldY = this.y;
+        }
+    };
+
+    update(paused) {
+        if (paused) {
+            return;
+        }
+        this.draw();
+    };
 }
-
-Paddle.prototype = Object.create(RectImage.prototype);
-Paddle.prototype.constructor = Paddle;
-
-Paddle.prototype.onDragTo = function (x) {
-   this.moveTo(x-this.w/2, this.y);
-   this.draw();
-};
-Paddle.prototype.moveTo = function (x, y) {
-    let c = this.ctx.canvas;
-    this.x = numInSection(x, 0, c.width - this.w);
-    this.y = y;
-    this.reDraw = (this.x !== this.oldX || this.y !== this.oldY);
-};
-
-Paddle.prototype.moveLeft = function () {
-    this.moveTo(this.x - this.speedX, this.y);
-};
-Paddle.prototype.movedRight = function () {
-    this.moveTo(this.x + this.speedX, this.y);
-};
-
-Paddle.prototype.onCollide = function () {
-    //log(area);
-    this.reDraw = true;
-};
-
-Paddle.prototype.draw = function () {
-    if (this.reDraw) {
-        // log('paddle:redraw.');
-        this.clearSelf();
-        let c = this.ctx;
-        c.fillStyle = this.color;
-        c.fillRect(this.x, this.y, this.w, this.h);
-        this.reDraw = false;
-        this.oldX = this.x;
-        this.oldY = this.y;
-    }
-};
-
-Paddle.prototype.update = function (paused) {
-    if (paused) {
-        return;
-    }
-    this.draw();
-};
