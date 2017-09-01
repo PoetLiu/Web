8d8b1ef2c0bb797d937bf5a523d8dd88e4daa093
Albert Liu <457566268@qq.com>
2017-09-01 09:37:09 +0800

    Optimize: Change code to ECMAScript6.0. Using Class.

--- BreakTheBricks/js/Audio.js
+++ BreakTheBricks/js/Audio.js
@@ -1,17 +1,19 @@
-function Audio(src, loop) {
-    let a = document.createElement('audio');
-    a.src   = src;
-    a.loop  = loop;
-    a.load();
-    this.audio = a;
-    this.src = src;
-    this.loop    = loop;
+class Audio {
+    constructor(src, loop) {
+        let a = document.createElement('audio');
+        a.src = src;
+        a.loop = loop;
+        a.load();
+        this.audio = a;
+        this.src = src;
+        this.loop = loop;
+    };
+
+    play() {
+        this.audio.play();
+    };
+
+    pause() {
+        this.audio.pause();
+    };
 }
-
-Audio.prototype.play = function () {
-    this.audio.play();
-};
-
-Audio.prototype.pause = function () {
-    this.audio.pause();
-};
