b0a95615a6bb2a1a4243a0bf7ea76f43eacebd41
PoetLiu <457566268@qq.com>
2017-09-03 22:42:20 +0800

    Optimize: SceneStart call father's update func.

--- BreakTheBricks/js/SceneStart.js
+++ BreakTheBricks/js/SceneStart.js
@@ -1,26 +1,25 @@
 class SceneStart extends Scene {
     constructor(game) {
         super(game);
-        this.texts = [];
         this.bgAudio = null;
     };
 
     init() {
         let g = this.game;
-        this.texts.push(new Text(g.ctx, 220, 200, 50, 'black', 'serif', 1.0, function () {
+        this.addTexture(new Text(g.ctx, 220, 200, 50, 'black', 'serif', 1.0, function () {
             return 'Break The Bricks';
         }));
-        this.texts.push(new Text(g.ctx, 220, 280, 30, 'black', 'serif', 0.8, function () {
+        this.addTexture(new Text(g.ctx, 220, 280, 30, 'black', 'serif', 0.8, function () {
             return 'Press j to move left the paddle.';
         }));
-        this.texts.push(new Text(g.ctx, 220, 310, 30, 'black', 'serif', 0.8, function () {
+        this.addTexture(new Text(g.ctx, 220, 310, 30, 'black', 'serif', 0.8, function () {
             return 'Press k to move right the paddle.';
         }));
-        this.texts.push(new Text(g.ctx, 220, 340, 30, 'black', 'serif', 0.8, function () {
+        this.addTexture(new Text(g.ctx, 220, 340, 30, 'black', 'serif', 0.8, function () {
             return 'Press s to start game.';
         }));
         if (g.debugMode) {
-            this.texts.push(new Text(g.ctx, 220, 370, 30, 'black', 'serif', 0.8, function () {
+            this.addTexture(new Text(g.ctx, 220, 370, 30, 'black', 'serif', 0.8, function () {
                 return 'Press space to pause/continue game.';
             }));
         }
@@ -39,9 +38,6 @@ class SceneStart extends Scene {
 
     update() {
         // log('SceneStart update.');
-        for (let i = 0; i < this.texts.length; i++) {
-            let t = this.texts[i];
-            t.update();
-        }
+        super.update();
     };
 }
