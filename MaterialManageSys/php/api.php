902f202965a93d8258b5454b186ee2ded90a1570
PoetLiu <457566268@qq.com>
2017-12-12 21:48:22 +0800

    New: Split uploadCheck function to individual file.

--- MaterialManageSys/php/api.php
+++ MaterialManageSys/php/api.php
@@ -4,27 +4,13 @@ include 'PHPExcel/Classes/PHPExcel.php';
 include 'upload.php';
 include 'getBomData.php';
 include 'getStock.php';
+include 'uploadCheck.php';
 
 $uploadDir = '/tmp/';
 function init()
 {
     ini_set('display_errors', 'On');
     error_reporting(E_ALL ^ (E_NOTICE | E_WARNING));
-}
-
-function upload_check()
-{
-    $file = $GLOBALS['uploadDir'] . $_POST['name'];
-    $res = new stdClass();
-    if (file_exists($file)) {
-        $res->errCode = 0;
-        $res->msg = "File is valid, and was successfully uploaded.";
-    } else {
-        $res->errCode = 1;
-        $res->msg = "File upload failed!.";
-    }
-//    print_r($_POST);
-    print json_encode($res);
 }
 
 init();
@@ -34,8 +20,8 @@ if ($api == 'getStock') {
     getStock();
 } else if ($api == 'upload') {
     upload();
-} else if ($api == 'upload_check') {
-    upload_check();
+} else if ($api == 'uploadCheck') {
+    uploadCheck();
 } else if ($api == 'getBomData') {
     getBomData();
 }
