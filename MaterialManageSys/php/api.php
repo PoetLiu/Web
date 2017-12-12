42d7bd8f0803d720a339089454c942f512f1513b
PoetLiu <457566268@qq.com>
2017-12-12 21:45:36 +0800

    New: Split getStock function to individual file.

--- MaterialManageSys/php/api.php
+++ MaterialManageSys/php/api.php
@@ -3,6 +3,7 @@
 include 'PHPExcel/Classes/PHPExcel.php';
 include 'upload.php';
 include 'getBomData.php';
+include 'getStock.php';
 
 $uploadDir = '/tmp/';
 function init()
@@ -10,35 +11,6 @@ function init()
     ini_set('display_errors', 'On');
     error_reporting(E_ALL ^ (E_NOTICE | E_WARNING));
 }
-
-function getStock()
-{
-    $serverName = 'localhost';
-    $username = 'root';
-    $password = 'liupeng123';
-    $dbName = 'material';
-
-    // create connection.
-    $conn = new mysqli($serverName, $username, $password, $dbName);
-    if ($conn->connect_error) {
-        die('Connection failed: ' . $conn->connect_error);
-    }
-    $conn->set_charset('utf8mb4');
-
-    $sql = "SELECT * FROM test";
-    $result = $conn->query($sql);
-    $rows = array();
-    if ($result->num_rows > 0) {
-        // output data of each row.
-        while ($row = $result->fetch_assoc()) {
-            $rows[] = $row;
-        }
-    }
-//    echo "Hello, world!";
-    print json_encode($rows);
-    $conn->close();
-}
-
 
 function upload_check()
 {
