e3b76c3f164afd9bc4aee49e3af4ac966ed460cb
Albert Liu <457566268@qq.com>
2018-01-05 15:07:48 +0800

    New: Add login cgi call.

--- Router/js/login.js
+++ Router/js/login.js
@@ -1,3 +1,43 @@
 $(document).ready(function () {
     $("#login-input").focus();
+    $("#login-btn").on("click", login);
 });
+var errNo = [
+    "48",
+    "49",
+    "50",
+    "51",
+    "52"
+];
+
+function login() {
+    if ($.cookie("Qihoo_360_login")) {
+        $.cookie("Qihoo_360_login", null, {
+            path: "/",
+            expires: 1
+        });
+    }
+
+    var login = {};
+    login.name = 'admin';
+    login.pass = aesEncrypt($("#login-input").val());
+    login.from = 1;
+
+    console.log(login);
+    $.post("/router/web_login.cgi", login, function (data) {
+        try {
+            data = eval("(" + data + ")");
+            if (data["success"] == "1" && data["token_id"] != "") {
+                location.href = "./index.html?token_id=" + data["token_id"];
+            } else {
+                if (!isNaN(data.err_no) && errNo.indexOf(data.err_no) !== -1) {
+                    showMessage("login_failure" + data.err_no);
+                } else {
+                    showMessage("login_failure");
+                }
+            }
+        } catch (e) {
+            showMessage("login_failure");
+        }
+    });
+}
--- Router/js/utility.js
+++ Router/js/utility.js
@@ -1,6 +1,6 @@
 function funCall(f, param) {
     console.log(param);
-    if (f != null && f !== "")  {
+    if (f != null && f !== "") {
         try {
             if (typeof(window[f]) === "function") {
                 window[f].apply(this, param);
@@ -22,15 +22,61 @@ function setCurClass(id, curName) {
 
 function loadHtml(html, init) {
     currentHtml = html;
-    var params  = Array.prototype.slice.apply(arguments, [2, arguments.length]);
+    var params = Array.prototype.slice.apply(arguments, [2, arguments.length]);
     $.ajax({
         type: "get",
-        url:    "./"+html+".html",
+        url: "./" + html + ".html",
         dataType: "html",
         success: function (ret) {
-            var container    = $("#container");
+            var container = $("#container");
             container.html(ret);
             funCall(init, params);
         }
     });
+}
+
+function get_rand_key() {
+    var calleeFn = arguments.callee;
+    var ret = {
+        "rand_key": "",
+        "key_index": ""
+    };
+
+    $.ajax({
+        url: "/router/get_rand_key.cgi",
+        data: {
+            "noneed": "noneed",
+            "key_index": ""
+        },
+        dataType: "json",
+        async: false,
+        error: function (XMLHttpRequest, textStatus) {
+            console.log(XMLHttpRequest, textStatus);
+        },
+        success: function (data) {
+            if (data.rand_key) {
+                ret["rand_key"] = data.rand_key.substring(32, 64);
+                ret["key_index"] = data.rand_key.substring(0, 32);
+            } else {
+                console.log("Get rand key error, data:"+data);
+            }
+        }
+    });
+    return ret;
+}
+
+function aesEncrypt(data, key) {
+    key = key || get_rand_key();
+    var keyHex  = CryptoJS.enc.Hex.parse(key.rand_key);
+    var iv  = CryptoJS.enc.Latin1.parse("360luyou#install");
+    var cipher  = CryptoJS.AES.encrypt(data, keyHex, {
+        iv: iv,
+        mode: CryptoJS.mode.CBC,
+        padding: CryptoJS.pad.Pkcs7
+    });
+    return key.key_index + cipher.ciphertext.toString();
+}
+
+function showMessage(title, message) {
+    console.log(title, message);
 }
