82b5613e67a39417d86413752251933a175de583
Albert Liu <457566268@qq.com>
2018-01-10 16:28:28 +0800

    New: Add power introduce text and select bar.

--- Router/app/radio_power/js/index.js
+++ Router/app/radio_power/js/index.js
@@ -3,22 +3,23 @@ $(document).ready(function () {
 });
 
 function powerBarUpdate(mode) {
-    var w   = 0;
-    switch (mode) {
-        case "low":
-            w = 40;
-            break;
-        case "middle":
-            w = 316;
-            break;
-        case "high":
-            w = 630;
-            break;
-        default:
-            console.log("Unknown mode:", mode);
-            break;
-    }
-    if (w) {
-        $("#pw-select").animate({width: w+"px"}, 500);
+    var modes = {
+        "low": {
+           width: 40,
+           intro: "2%的WiFi发射功率，准妈妈再也不用担心辐射问题"
+        },
+        "middle": {
+           width: 316,
+           intro: "WiFi发射功率50%，既能轻松上网，又能降低辐射"
+        },
+        "high": {
+           width: 630,
+           intro: "P+内核信号增益已开，手机WiFi信号较弱时自动增强发射功率，家中信号死角地区也能畅快上网"
+        }
+    };
+    var m   = modes[mode];
+    if (m) {
+        $("#pw-select").animate({width: m.width+"px"}, 500);
+        $("#pw-intro").html(m.intro);
     }
 }
