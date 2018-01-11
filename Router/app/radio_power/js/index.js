323eec1c0bfa1d76139d1878eecf942c84e582e7
Albert Liu <457566268@qq.com>
2018-01-11 14:33:05 +0800

    Mod: Finish power bar switch while mouse click.

--- Router/app/radio_power/js/index.js
+++ Router/app/radio_power/js/index.js
@@ -1,20 +1,26 @@
 $(document).ready(function () {
     powerBarUpdate("high");
+    $(".power .select-bar a").click(function (e) {
+        powerBarUpdate($(this).attr('mode'));
+    });
 });
 
 function powerBarUpdate(mode) {
     var modes = {
         "low": {
            width: 40,
-           intro: "2%的WiFi发射功率，准妈妈再也不用担心辐射问题"
+           intro: "2%的WiFi发射功率，准妈妈再也不用担心辐射问题",
+            power: 2
         },
         "middle": {
            width: 316,
-           intro: "WiFi发射功率50%，既能轻松上网，又能降低辐射"
+           intro: "WiFi发射功率50%，既能轻松上网，又能降低辐射",
+            power: 50
         },
         "high": {
            width: 630,
-           intro: "P+内核信号增益已开，手机WiFi信号较弱时自动增强发射功率，家中信号死角地区也能畅快上网"
+           intro: "P+内核信号增益已开，手机WiFi信号较弱时自动增强发射功率，家中信号死角地区也能畅快上网",
+            power: 100
         }
     };
     var m   = modes[mode];
