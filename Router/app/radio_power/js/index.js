8a25c3935a74b18f4570780377b34cf1d27e04c9
Albert Liu <457566268@qq.com>
2018-01-12 14:27:16 +0800

    Mod: Code optimize.

--- Router/app/radio_power/js/index.js
+++ Router/app/radio_power/js/index.js
@@ -8,26 +8,25 @@ $(document).ready(function () {
 function powerBarUpdate(mode) {
     var modes = {
         "low": {
-           width: 40,
-           intro: "2%的WiFi发射功率，准妈妈再也不用担心辐射问题",
+            width: 40,
+            intro: "2%的WiFi发射功率，准妈妈再也不用担心辐射问题",
             power: 2
         },
         "middle": {
-           width: 316,
-           intro: "WiFi发射功率50%，既能轻松上网，又能降低辐射",
+            width: 316,
+            intro: "WiFi发射功率50%，既能轻松上网，又能降低辐射",
             power: 50
         },
         "high": {
-           width: 630,
-           intro: "P+内核信号增益已开，手机WiFi信号较弱时自动增强发射功率，家中信号死角地区也能畅快上网",
+            width: 630,
+            intro: "P+内核信号增益已开，手机WiFi信号较弱时自动增强发射功率，家中信号死角地区也能畅快上网",
             power: 100
         }
     };
-    var m   = modes[mode];
+    var m = modes[mode];
     if (m) {
-        $("#pw-select").animate({width: m.width+"px"}, 500);
+        $("#pw-select").animate({width: m.width + "px"}, 500);
         $("#pw-intro").html(m.intro);
-        $("#pwa-"+mode).addClass("selected").siblings().removeClass("selected");
-
+        $("#pwa-" + mode).addClass("selected").siblings().removeClass("selected");
     }
 }
