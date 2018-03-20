4a93933e4ab1c22f377e067dd3d92fb571f109a1
liupeng <457566268@qq.com>
2018-03-20 10:32:39 +0800

    New: Adding wifi_set page init.

--- Router/js/wifi_set.js
+++ Router/js/wifi_set.js
@@ -1,3 +1,17 @@
 function initWifiSetPage() {
    showPathNav("我的安全路由", "WiFi设置");
+
+   paintView();
+   function paintView() {
+       var p = {
+           ap_id:       0,
+           network_mode:999,
+           port_id:     "WIFI1"
+       };
+
+       $.post("/router/wireless_base_show.cgi", p, function (data) {
+           var data = eval("("+data+")");
+           console.log(data);
+       });
+   }
 }
