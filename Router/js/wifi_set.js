b560aba63952fd1f767d4d201da73cf616d50b65
liupeng <457566268@qq.com>
2018-03-20 11:22:28 +0800

    New: Adding enable/ssid/channel/bandwidth display.

--- Router/js/wifi_set.js
+++ Router/js/wifi_set.js
@@ -1,8 +1,21 @@
 function initWifiSetPage() {
+    var dom = {
+        "AP_SSID":"#wifi_24g_ssid",
+        "wire_enable":"#wifi_24g_enable",
+        "channel_width":"#wifi_24g_bandwidth"
+    };
    showPathNav("我的安全路由", "WiFi设置");
 
-   paintView();
-   function paintView() {
+   initView();
+
+   function paintView(data) {
+        $.each(dom, function (id, item) {
+            console.log(id, item, data[id]);
+            $(item).val(data[id]);
+        });
+   }
+
+   function initView() {
        var p = {
            ap_id:       0,
            network_mode:999,
@@ -11,7 +24,7 @@ function initWifiSetPage() {
 
        $.post("/router/wireless_base_show.cgi", p, function (data) {
            var data = eval("("+data+")");
-           console.log(data);
+           paintView(data);
        });
    }
 }
