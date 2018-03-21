45d8a650a7a74595f36f58e9ba2358744b8a8219
liupeng <457566268@qq.com>
2018-03-21 14:55:13 +0800

    Mod: Add key row.

--- Router/js/wifi_set.js
+++ Router/js/wifi_set.js
@@ -2,16 +2,22 @@ function initWifiSetPage() {
     var dom = {
         "AP_SSID":"#wifi_24g_ssid",
         "wire_enable":"#wifi_24g_enable",
-        "channel_width":"#wifi_24g_bandwidth"
+        "channel_width":"#wifi_24g_bandwidth",
+        "ap_mode":"#wifi_24g_encrypt_mode",
+        "password":"#wifi_24g_pwd"
     };
    showPathNav("我的安全路由", "WiFi设置");
 
    initView();
 
+   var wifi_cfg;
+
    function paintView(data) {
         $.each(dom, function (id, item) {
             console.log(id, item, data[id]);
-            $(item).val(data[id]);
+            if (data[id]) {
+                $(item).val(data[id]);
+            }
         });
    }
 
@@ -19,11 +25,17 @@ function initWifiSetPage() {
        var p = {
            ap_id:       0,
            network_mode:999,
-           port_id:     "WIFI1"
+           port_id:     "WIFI1",
+           ap_mode:     0
        };
 
        $.post("/router/wireless_base_show.cgi", p, function (data) {
-           var data = eval("("+data+")");
+           data = eval("("+data+")");
+           paintView(data);
+       });
+
+       $.post("/router/wireless_sec_show.cgi", p, function (data) {
+           data = eval("("+data+")");
            paintView(data);
        });
    }
