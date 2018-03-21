0f1a0bcdc8d0b96080047676981582f589557c93
PoetLiu <457566268@qq.com>
2018-03-21 22:33:58 +0800

    New: Finish wifi enable and hide ssid show.

--- Router/js/wifi_set.js
+++ Router/js/wifi_set.js
@@ -1,23 +1,51 @@
 function initWifiSetPage() {
     var dom = {
-        "AP_SSID":      "#wifi_24g_ssid",
-        "wire_enable":  "#wifi_24g_enable",
-        "channel_width":"#wifi_24g_bandwidth",
-        "ap_mode":      "#wifi_24g_encrypt_mode",
-        "password":     "#wifi_24g_pwd",
-        "channel_num":  "#wifi_24g_channel"
+        "AP_SSID":      {
+            id:"#wifi_24g_ssid"
+        },
+        "wire_enable":  {
+            id:"#wifi_24g_enable",
+            val: function (v) {
+                if (v === "1") {
+                    $(this.id).removeClass("radio_off").addClass("radio_on");
+                } else {
+                    $(this.id).removeClass("radio_on").addClass("radio_off");
+                }
+            }
+        },
+        "channel_width": {
+            id:"#wifi_24g_bandwidth"
+        },
+        "ap_mode":      {
+            id:"#wifi_24g_encrypt_mode"
+        },
+        "password":     {
+            id:"#wifi_24g_pwd"
+        },
+        "channel_num":   {
+            id:"#wifi_24g_channel"
+        },
+        "SSID_broadcast": {
+            id:"#wifi_24g_hide_ssid",
+            val: function (v) {
+                $(this.id).prop("checked", v==="0");
+            }
+        }
     };
    showPathNav("我的安全路由", "WiFi设置");
 
    initView();
 
    var wifi_cfg;
-
    function paintView(data) {
         $.each(dom, function (id, item) {
             // console.log(id, item, data[id]);
             if (data[id] !== undefined) {
-                $(item).val(data[id]);
+                if (item.val) {
+                    item.val(data[id]);
+                } else {
+                    $(item.id).val(data[id]);
+                }
             }
         });
    }
